1210b39b2453a78dc6038a6161c1a3e6c4602708
AvinandanRoy <abhinandanroylipi@gmail.com>
2024-03-11 21:09:20 +0600

    Increment button completed

--- Module-29/Shopping_Cart/JS/script.js
+++ Module-29/Shopping_Cart/JS/script.js
@@ -1,32 +1,58 @@
 // subtotal , tax , total 
 // product-1 : p1Price,p1Increase,p1Decrease,p1Quantity,p1TotalPrice
 // product-2 : p2Price ,
-const price =(quantity)=>{
-    const prevPrice= parseFloat(document.getElementById('p1TotalPrice'))
-    prevPrice = 0 ;
-    const perPrice = parseFloat(document.getElementById('p1Price'));
-    const p1TotalPrice = document.getElementById('p1TotalPrice').innerHTML;
-    p1TotalPrice = prevPrice + (quantity * perPrice);
-    return p1TotalPrice;
+
+const p1IncreBtn = document.getElementById('p1Increase');
+const p1DecreBtn = document.getElementById('p1Decrease')
+const p1Price = document.getElementById('p1Price');
+
+const p1TotalPrice = document.getElementById('p1TotalPrice');
+const p1Quantity = document.getElementById('p1Quantity');
+
+const subtotal = document.getElementById('subtotal');
+const total = document.getElementById('total');
+
+
+const increaseQuntatity =()=>{
+    const quantity = parseFloat(p1Quantity.innerHTML)+ 1 ;
+    return quantity;
+}
+const increasePrice= ()=>{
+    p1TotalPrice.innerHTML = 0 ;
+    const quantity = increaseQuntatity()-1;
+    p1TotalPrice.innerHTML = quantity* parseFloat(p1Price.innerHTML);
+    return p1TotalPrice.innerHTML;
 }
 
-const incrementButton =(quantity) =>{
-    const tempQuantity = parseFloat(quantity) ;
-    return tempQuantity+1;
-}
-const decrementButton =(quantity)=>{
-    const tempQuantity = parseFloat(quantity);
-    const nextQuantity = tempQuantity- 1;
-    return nextQuantity ;
+const subTotals =()=>{
+    subtotal.innerHTML = increasePrice();
+    return subtotal.innerHTML ;
 }
 
-document.getElementById('p1Increase').addEventListener('click',()=>{
-    const quantity = document.getElementById('p1Quantity').innerHTML;
-    document.getElementById('p1Quantity').innerHTML = incrementButton(quantity,);
-    console.log(parseFloat(quantity)+1);
+const taxes =()=>{
+    const eachTax = 500 ;
+    const quantity = increaseQuntatity()-1;
+    let totalTax = 0 ;
+    for(let i= 0 ; i < quantity ;i++){
+        totalTax = eachTax * (i+1);
+    }
+    tax.innerHTML = totalTax;
+    return tax.innerHTML ;
+}
+
+const totalPrice =()=>{
+    total.innerHTML = parseFloat(subTotals()) + parseFloat(taxes());
+}
+
+p1IncreBtn.addEventListener('click',()=>{
+    p1Quantity.innerHTML= increaseQuntatity();
+    increasePrice();
+    subTotals();
+    taxes();
+    totalPrice();
 })
 
-document.getElementById('p1Decrease').addEventListener('click',()=>{
-    const quantity = document.getElementById('p1Quantity').innerHTML;
-    document.getElementById('p1Quantity').innerHTML = decrementButton(quantity);
+
+p1DecreBtn.addEventListener('click',(event)=>{
+
 })
